996423266603a17bcf717352c7d6e1d95f175ad0
Tânia de Arruda <tandearruda@gmail.com>
2021-08-24 18:15:43 -0300

    Fix Session and User routes

--- src/controllers/SessaoController.js
+++ src/controllers/SessaoController.js
@@ -1,22 +1,27 @@
 const con = require('../database/database')
 
 module.exports = {
-    login: async (req, res) => {
-        const { email, senha } = req.body
-        const [usuario] = await con('usuario').select('*').where({ email }).limit(1)
+    login: async (req, res, next) => {
+        try {
+            const { email, senha } = req.body
+            const [usuario] = await con('usuario').select('*').where({ email }).limit(1)
 
-        if (!usuario) {
-            return res.status(403).json({ error: 'Usuário não encontrado' })
+            if (!usuario) {
+                return res.status(403).json({ error: 'Usuário não encontrado' })
+            }
+
+            if (senha !== usuario.senha) {
+                return res.status(403).json({ error: 'Senha'})
+            }
+
+            return res.json({
+                id: usuario.id,
+                email,
+                token: 'sdjfaiuh48547adsjaoj54l5'
+            })
+        } catch (error) {
+            next(error)
         }
-
-        if (senha !== usuario.senha) {
-            return res.status(403).json({ error: 'Senha'})
-        }
-
-        return res.json({
-            id: usuario.id,
-            email,
-            token: 'sdjfaiuh48547adsjaoj54l5'
-        })
+        
     }
 }
