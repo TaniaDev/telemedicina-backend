ed1ece009ae33b9481b99e00de0bbafbf7926d77
William <willrodrigues4@gmail.com>
2021-08-24 18:09:36 -0300

    Registros Seeds Usuario

--- src/database/seeds/001_usuario.js
+++ src/database/seeds/001_usuario.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('usuario').insert([
-        { nome: 'rowValue1' },
-        { nome: 'rowValue2' },
-        { nome: 'rowValue3' }
+        { nome: 'rowValue1', dt_nascimento: '2021-01-01', genero: 'masculino', email: 'bla@bla.com',senha: '123', created_at: null, updated_at: null, deleted_at: null },
+        { nome: 'rowValue2', dt_nascimento: '2020-01-01', genero: 'feminino', email: 'bla2@bla.com',senha: '321', created_at: null, updated_at: null, deleted_at: null },
+        { nome: 'rowValue3', dt_nascimento: '2019-01-01', genero: 'masculino', email: 'bla3@bla.com',senha: '246', created_at: null, updated_at: null, deleted_at: null }
       ]);
     });
 };
