e9b64741bb73751414b0e491db3880052c3064b5
Tânia de Arruda <tandearruda@gmail.com>
2021-08-25 20:25:06 -0300

    Fix Create User route

--- src/database/seeds/001_usuario.js
+++ src/database/seeds/001_usuario.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('usuario').insert([
-        { nome: 'rowValue1', dt_nascimento: '2021-01-01', genero: 'masculino', email: 'bla@bla.com',senha: '123', created_at: null, updated_at: null },
-        { nome: 'rowValue2', dt_nascimento: '2020-01-01', genero: 'feminino', email: 'bla2@bla.com',senha: '321', created_at: null, updated_at: null },
-        { nome: 'rowValue3', dt_nascimento: '2019-01-01', genero: 'masculino', email: 'bla3@bla.com',senha: '246', created_at: null, updated_at: null }
+        { nome: 'rowValue1', dt_nascimento: '2021-01-01', genero: 'H', email: 'bla@bla.com',senha: '123', created_at: null, updated_at: null },
+        { nome: 'rowValue2', dt_nascimento: '2020-01-01', genero: 'M', email: 'bla2@bla.com',senha: '321', created_at: null, updated_at: null },
+        { nome: 'rowValue3', dt_nascimento: '2019-01-01', genero: 'H', email: 'bla3@bla.com',senha: '246', created_at: null, updated_at: null }
       ]);
     });
 };
