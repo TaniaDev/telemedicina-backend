b5e551af6571efa0773b44cf4affd92f823732d6
Tania de Arruda <tandearruda@gmail.com>
2022-04-25 16:05:32 -0300

    Atualiza autenticação dos papéis de usuário

--- src/middlewares/patient.js
+++ src/middlewares/patient.js
@@ -1,15 +1,10 @@
 const jwt_decode = require('jwt-decode')
 
-const con = require('../database')
-
-
 module.exports = async (req, res, next) => {
     const authHeader = req.headers.authorization
     const decode = jwt_decode(authHeader)
 
-    const [usuario] = await con('usuario').where({ id: decode.id })
-
-    if (usuario.tipo != 'Paciente') {
+    if (decode.tipo !== 'Paciente') {
         return res.status(401).json({ error: 'Acesso negado!' })
     }
 
