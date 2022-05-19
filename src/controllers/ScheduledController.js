da2990661a6f2fdd4f1e0c97cb24ed526832776d
William <willrodriguess77@gmail.com>
2022-05-19 14:08:10 -0300

    Melhorando email notificacao

--- src/controllers/ScheduledController.js
+++ src/controllers/ScheduledController.js
@@ -7,20 +7,22 @@ module.exports = {
             let current = dayjs().format('YYYY-MM-DD HH:mm:00')
             let oneMoreDay = dayjs().add(1, 'day').format('YYYY-MM-DD HH:mm:00')
             let oneMoreHour = dayjs().add(1, 'hour').format('YYYY-MM-DD HH:mm:00')
-            let fifteenMoreMinutes = dayjs().add(15, 'minute').format('YYYY-MM-DD HH:mm:00')     
-
-            const results = await con('consulta').select('*')
+            let fifteenMoreMinutes = dayjs().add(15, 'minute').format('YYYY-MM-DD HH:mm:00') 
+            
+            const results = await con('consulta').select('usuario.email', 'usuario.nome', 'usuario2.nome as doctor', 'especialidade.nome as specialty', 'consulta.dt_hr_consulta')
                 .join('usuario', 'usuario.id', '=', 'consulta.id_paciente')
+                .join('usuario as usuario2', 'usuario2.id', '=', 'consulta.id_medico')
+                .join('especialidade', 'especialidade.id', '=', 'consulta.id_especialidade')
                 .where('consulta.dt_hr_consulta', '>', current)
                 .andWhere('consulta.status', '=', 'Agendado')
-
+    
             results.map((result) => {  
                 if(dayjs(result.dt_hr_consulta).format('YYYY-MM-DD HH:mm:00') == oneMoreDay){
-                    require('../modules/notification')(result.email, result.nome, 24, 'horas')
+                    require('../modules/notification')(result.email, result.nome, 24, 'horas', result.doctor, result.specialty, dayjs(result.dt_hr_consulta).format('DD/MM/YYYY HH:mm:00'))
                 }else if(dayjs(result.dt_hr_consulta).format('YYYY-MM-DD HH:mm:00') == oneMoreHour){
-                    require('../modules/notification')(result.email, result.nome, 1, 'hora')
+                    require('../modules/notification')(result.email, result.nome, 1, 'hora', result.doctor, result.specialty, dayjs(result.dt_hr_consulta).format('DD/MM/YYYY HH:mm:00'))
                 }else if(dayjs(result.dt_hr_consulta).format('YYYY-MM-DD HH:mm:00') == fifteenMoreMinutes){
-                    require('../modules/notification')(result.email, result.nome, 15, 'minutos')
+                    require('../modules/notification')(result.email, result.nome, 15, 'minutos', result.doctor, result.specialty, dayjs(result.dt_hr_consulta).format('DD/MM/YYYY HH:mm:00'))
                 }
             })
         
--- src/modules/notification.js
+++ src/modules/notification.js
@@ -2,8 +2,7 @@
 const nodemailer = require('nodemailer')
 const { host, port, user, pass } = require('../config/mail.json')
 
-module.exports = (email, nome, time, precision) => {
-    
+module.exports = (email, nome, time, precision, doctor, specialty, date) => {
     const smtpTransport = nodemailer.createTransport({
         host,
         port,
@@ -13,8 +12,13 @@ module.exports = (email, nome, time, precision) => {
     const mail = {
         from: "Telemedicina <fatec.telemedicina@gmail.com>",
         to: email,
-        subject: `${nome}, Faltam Menos de ${time} ${precision} para sua Consulta`,
-        html: `Sua Consulta se aproxima, fique Atento!`
+        subject: `${nome}, faltam menos de ${time} ${precision} para sua consulta`,
+        html: `
+                <h2 align="center">Sua consulta se aproxima, fique atento!</h2>
+                <p align="center"><b>Médico:</b> ${doctor}</p>
+                <p align="center"><b>Especialidade:</b> ${specialty}</p>
+                <p align="center"><b>Data:</b> ${date}</p>
+            `
     }
     
     return new Promise((resolve, reject) => {
